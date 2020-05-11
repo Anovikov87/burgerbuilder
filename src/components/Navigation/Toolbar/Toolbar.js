3b88bb045f6b63a7beac80373a014de3190f0fda
Anovikov <dang3rouz@ya.ru>
2020-05-11 11:54:55 +0500

    finish logo sizing

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,13 +5,15 @@ import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
-     <header className={classes.Toolbar}>
-         <div>MENU</div>
-         <Logo height="80%"/>
-         <nav>
-             <NavigationItems></NavigationItems>
-         </nav>
-     </header>
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav>
+            <NavigationItems></NavigationItems>
+        </nav>
+    </header>
 )
 
 export default toolbar
